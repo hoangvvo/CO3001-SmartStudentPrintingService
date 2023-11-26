61d917b335a96bd8e32228e5e83649601a65a5f9
Hoang Vo <dev+g@hvvo.dev>
2023-11-26 10:59:48 +0700

    Scaffold pages

---
+++ frontend/src/app/files/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+export default function FilesPage() {
+  return (
+    <div className="container">
+      <div className="flex py-4 justify-between items-center">
+        <div className="space-y-0.5">
+          <h2 className="text-2xl font-bold tracking-tight">My Files</h2>
+          <p className="text-muted-foreground">
+            Manage all your files in one place
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ frontend/src/app/payments/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+export default function PaymentsPage() {
+  return (
+    <div className="container">
+      <div className="flex py-4 justify-between items-center">
+        <div className="space-y-0.5">
+          <h2 className="text-2xl font-bold tracking-tight">Payments</h2>
+          <p className="text-muted-foreground">Your payments</p>
+        </div>
+      </div>
+    </div>
+  );
+}
