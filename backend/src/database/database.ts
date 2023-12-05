a1255618b84c81172a134fa1508a7b640c833ee9
Hoang <dev+g@hvvo.dev>
2023-12-06 01:23:42 +0700

    fix download

--- backend/src/database/database.ts
+++ backend/src/database/database.ts
@@ -4,6 +4,6 @@ import { DATABASE_URL } from "../constants/environments.js";
 export const pool = new pg.Pool({
   max: 20,
   idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 2000,
+  connectionTimeoutMillis: 10000,
   connectionString: DATABASE_URL,
 });
