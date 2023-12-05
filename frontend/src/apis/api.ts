d765b91292c3e9c0688e79bb9f0ecb9f808bf590
Hoang <dev+g@hvvo.dev>
2023-12-06 00:43:59 +0700

    direct api to server

--- frontend/src/apis/api.ts
+++ frontend/src/apis/api.ts
@@ -1,6 +1,6 @@
 import { Configuration, DefaultApi } from "./openapi";
 
-export const API_BASE_PATH = "/api";
+export const API_BASE_PATH = process.env.NEXT_PUBLIC_API_URL;
 
 export const api = new DefaultApi(
   new Configuration({
